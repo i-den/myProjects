68134b267f8e22bb95baa817c12866283cc34721
i-den <idenchev07@gmail.com>
2017-07-29 20:54:34 +0300

    Added /user/register HTML

--- ExpressJS-CatShopApp/models/User.js
+++ ExpressJS-CatShopApp/models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 let userSchema = mongoose.Schema(
     {
-        name: {type: String, required: true},
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true},
         email: {type: String, required: true, unique: true},
         passwordHash: {type: String, required: true},
         salt: {type: String, required: true},
